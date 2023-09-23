3793f81b0bd26c40c1619bcb37c1f58103993f77
marekzytko <marek.zytko@pm.me>
2023-09-24 00:27:19 +0200

    deez nuts
    - status indicators added to mega.sh and dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+echo -ne "Copying configuration files..."
 cp ./dotfiles/wallpaper.jpg ~/.config/i3/
 cp ./dotfiles/config ~/.config/i3/
-
+echo -e "\e[42mSUCCESS\e[0m"
 
 
--- mega.sh
+++ mega.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+echo -ne "Installing MegaSync..."
+
 wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb
 sudo apt install -y "$PWD/megasync-xUbuntu_22.04_amd64.deb"
+
+echo -e "\e[42mSUCCESS\e[0m"
