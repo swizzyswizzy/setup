d5e2bdfd80a899751d329a2f496a0743c074216f
swizzyswizzy <marek.zytko@pm.me>
2023-09-30 02:51:46 +0200

    emacs init.el file added, direct copying files script modified
    emacs init.el file added, direct copying files script modified

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "copying files from CONFIG_FILES to repository..."
-cp -r ~/CONFIG_FILES/* ~/setup/dotfiles
-
 echo "copying dotfiles (.config directory) from repository to ~/.config/ directory:"
 cp -r ./dotfiles/* ~/.config
 
