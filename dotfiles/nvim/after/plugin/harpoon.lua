a16fb03cfb643147356c4c8140044eb3687119e9
swizzyswizzy <marek.zytko@pm.me>
2023-10-19 17:37:45 +0200

    nav_file shortcut fixed (harpoon) nav_file shortcut fixed (harpoon)

--- dotfiles/nvim/after/plugin/harpoon.lua
+++ dotfiles/nvim/after/plugin/harpoon.lua
@@ -6,6 +6,6 @@ vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
 -- TODO change keystrokes 
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(6) end)
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
