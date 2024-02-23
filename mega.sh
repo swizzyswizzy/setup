1bf61ca1cff5cc05bbd9ed11f678e0b8444298f1
swizzyswizzy <marek.zytko@pm.me>
2024-02-23 02:42:36 +0100

    removing complexity removing complexity

--- mega.sh
+++ mega.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#TODO
-#- add parameters (--ssh, --i3, --upgrade, etc.)
-#- errors logging
-#- split sections into seperate files (file for packages, file for ssh, etc)
-# if "--option or -all"
 echo -ne "Installing MegaSync..."
 
-wget https://mega.nz/linux/repo/xUbuntu_22.04/amd64/megasync-xUbuntu_22.04_amd64.deb
-sudo apt install -y "$PWD/megasync-xUbuntu_22.04_amd64.deb"
+wget https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst && sudo pacman -U megasync-x86_64.pkg.tar.zst && rm megasync-x86_64.pkg.tar.zst
 
 echo -e "\e[42mSUCCESS\e[0m"
--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 SSH_KEY=~/.ssh/marekzytko_key
 
 # List of packages:
-packages=("git" "i3" "curl" "gimp" "chromium" "keepass" "tmux" "zsh" "sl" "ripgrep" "rofi" "tldr")
+packages=("git" "gimp" "keepass" "tmux" "zsh" "ripgrep" "rofi" "tldr" "falkon" "fcron" "openvpn")
 
 
 if [ "$EUID" -eq 0 ]
@@ -14,53 +14,19 @@ fi
 
 
 
-#						DETERMINING PACKAGE MANAGER
-# ----------------------------------------------------------------------------------------------
-# https://ilhicas.com/2018/08/08/bash-script-to-install-packages-multiple-os.html
-
-declare -A osInfo;
-
-osInfo[/etc/debian_version]="apt-get install -y"
-osInfo[/etc/alpine-release]="apk --update add"
-osInfo[/etc/centos-release]="yum install -y"
-osInfo[/etc/fedora-release]="dnf install -y"
-osInfo[/etc/arch-release]="pacman -Syy"
-
-for f in ${!osInfo[@]};
-do
-	if [[ -f $f ]];
-	then
-		PACKAGE_MANAGER=${osInfo[$f]}
-		echo Package manager is: \>\> $PACKAGE_MANAGER \<\<\n\n
-	fi
-done
-# ----------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-#						.SH SCRIPTS
-# ----------------------------------------------------------------------------------------------
+#.SH SCRIPTS
 source dotfiles.sh
 source mega.sh
 source zsh.sh
-# ----------------------------------------------------------------------------------------------
 
 
 
-
-#						INSTALLING PACKAGES
-# ----------------------------------------------------------------------------------------------
 
 # Install packages list:
 for package in ${packages[@]};
 do
 	echo -n "Installing ${package}..."
-	sudo ${PACKAGE_MANAGER} ${package} > /dev/null
+	sudo pacman -Syy ${package} > /dev/null
 	if [ $? -eq 0 ];
 	then
 		echo -e "\e[42mSUCCESS\e[0m"
@@ -70,17 +36,9 @@ do
 		
 	fi
 done
-# ----------------------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-#						SSH SETUP
-# ----------------------------------------------------------------------------------------------
+# SSH SETUP
 # Setup ssh-agent and generate ssh key
 echo -n "starting SSH agent..." 
 eval $(ssh-agent) > /dev/null
@@ -89,7 +47,6 @@ if [ $? -eq 0 ];
 then
 	echo -e "\e[42mSUCCESS\e[0m"
 fi
-
 
 echo -n "generating SSH key..."
 # https://stackoverflow.com/questions/43235179/how-to-execute-ssh-keygen-without-prompt
@@ -101,5 +58,13 @@ echo -n "adding SSH key to agent..."
 ssh-add $SSH_KEY > /dev/null
 echo -e "\e[42mSUCCESS\e[0m"
 
-# ----------------------------------------------------------------------------------------------
+
+
+
+
+# Additional configuration commands:
+
+git config --global core.editor vim
+
+
 
