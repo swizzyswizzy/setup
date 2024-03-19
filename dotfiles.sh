20ff1e4143985226844ca547cb670ef20acd053c
swizzyswizzy <marek.zytko@pm.me>
2024-03-19 13:14:57 +0100

    Renamed dotfiles directories
    - Gitignore - added yay/ directory
    - Dotfiles.sh - copying directories modified accordingly (changed
      configuration files directories)
    - Plug.vim and solarized.vim removed
    - laptopSetup.sh - bunch of commands to customize OS experience when
      using OS on a laptop

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
 echo "copying dotfiles (.config directory) from repository to ~/.config/ directory:"
-cp -r ./dotfiles/* ~/.config
-
-echo "copying vim configuration file"
-cp ./dotfiles/ ~/.vim
-
-
-
-cp ./dotfiles/.tmux.conf ~/
-cp ./dotfiles/.zshrc ~/
-
+cp -r ./dotfiles/.config ~/.config
+cp -r ./dotfiles/homedir ~
 
 
 echo -e "\e[42mSUCCESS\e[0m"
---
+++ laptopSetup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Setting 60 second screen poweroff
+xset s 60 60
+
+
+# Touchpad natural scrolling and tap to click
+#
+# TODO
+# TBD
+
+
+#  
