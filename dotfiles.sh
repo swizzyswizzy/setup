e22aab29a710060fe93e3c5053c392184dabbe0f
swizzyswizzy <mail@mail.com>
2024-12-07 18:05:13 +0100

    dotfiles script fix and additional packaged
    dotfiles script fix and additional packaged

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "copying dotfiles (.config directory) from repository to ~/.config/ directory:"
-cp -r ./dotfiles/.config ~/.config
-cp -r ./dotfiles/homedir ~
+cp -r ./dotfiles/.config/* ~/.config
+cp -r ./dotfiles/homedir/* ~
 
 
 echo -e "\e[42mSUCCESS\e[0m"
