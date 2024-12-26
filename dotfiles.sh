310f0c1854e1cf7b7c8c4fe941f6577a294ef112
swizzyswizzy <marek.zytko@pm.me>
2024-12-26 22:50:06 +0100

    Copying xorg.conf.d files
    Copying xorg.conf.d files

--- dotfiles.sh
+++ dotfiles.sh
@@ -4,5 +4,8 @@ echo "copying dotfiles (.config directory) from repository to ~/.config/ directo
 cp -r ./dotfiles/.config/* ~/.config
 cp -r ./dotfiles/homedir/* ~
 
+#xorg.conf files
+cp -r ./dotfiles/xorg.conf.d/* /etc/X11/xorg.conf.d/
+
 
 echo -e "\e[42mSUCCESS\e[0m"
